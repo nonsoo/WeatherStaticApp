954025e5216bdbc9312841126f75451edc2521aa
Nonso <nonsootoh@gmail.com>
2021-05-31 10:45:28 -0400

    Initial Commit

---
+++ WeatherStaticApp/WeatherStaticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherStaticAppApp.swift
+//  WeatherStaticApp
+//
+//  Created by Nonso Otoh on 2021-05-31.
+//
+
+import SwiftUI
+
+@main
+struct WeatherStaticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
